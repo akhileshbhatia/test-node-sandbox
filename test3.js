e591e8f566c5fbd890ba5c4e2a986c8d652fb1d2
Akhilesh Bhatia <akhilesh.bhatia@siren.io>
2020-08-26 09:37:58 +0100

    Removes safe veal

--- test3.js
+++ test3.js
@@ -3,24 +3,20 @@ const { NodeVM } = require('vm2');
 class ABC {
 
     constructor() {
-        
-        this.nodeVM = new NodeVM();
-        this.output = { ...this, ...this.nodeVM } ;
+        this.nodeVM = new NodeVM({
+          sandbox: { getServices: this.getServices() }
+        });
+        this.input = { ...this, ...this.nodeVM } ;
     }
     
-    getServices = (param) => console.log('test value ', param);
-
-    // getServices = function (param) {
-    //     console.log('test value', param);
-    // }
+    getServices = (param) => {
+      console.log('test value ', param);
+    }
 
     vmRun = () => {
-        // const data = this.nodeVM.run('module.exports = function() {return {"a" : "b" }}');
-        const data2 = this.nodeVM.run.bind(this.output)('module.exports = this.getServices("some value")');
-        // console.log(data2);
+        const data2 = this.nodeVM.run('module.exports = getServices("some value")');
     }
 }
 
 const abcObj = new ABC();
-// abcObj.getServices();
 abcObj.vmRun();
